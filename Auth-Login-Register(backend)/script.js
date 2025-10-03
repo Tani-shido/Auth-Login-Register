bf709ef4465df65ec574fcd5cdb3f37024587a7e
Tanishq Dehariya <tanishqdehariya1892004@gmail.com>
2025-10-03 22:16:44 +0530

    Update script.js

--- Auth-Login-Register(backend)/script.js
+++ Auth-Login-Register(backend)/script.js
@@ -3,7 +3,21 @@ const app = express();
 
 app.put("/", (req,res)=>{
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = req.body.password;  
+});
 
+app.post("/", (req, res) => {
+    const userName = req.body.username;
+    const password = req.body.password;  
     
-})
+});
+app.get("/", (req, res) => {
+    const userName = req.body.username;
+    const password = req.body.password;  
+    
+});
+app.delete("/", (req, res) => {
+    const userName = req.body.username;
+    const password = req.body.password;  
+    
+});
