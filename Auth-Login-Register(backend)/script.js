49df343700d058e461c0bc36c9cb6b2e84a0a94d
Tani-shido <tanishqdehariya1892004@gmail.com>
2025-10-06 23:21:03 +0530

    Updated-Auth

--- Auth-Login-Register(backend)/script.js
+++ Auth-Login-Register(backend)/script.js
@@ -1,9 +1,50 @@
 const express = require("express");
 const app = express();
+import * as z from "zod";
+// const mongoose  = require("mongoose");
+// const { UserModel } = require("./db.js");
+app.use(express.json());
+const port = 3000;
 
-app.put("/", (req,res)=>{
+// mongoose.connect("mongodb+srv://shikaridota777:ZClPvfWjJINgzaFN@cluster0.12wqhve.mongodb.net/Auth-Login");
+
+app.post("/Signin", (req,res)=>{
     const userName = req.body.username;
-    const password = req.body.password;
+    console.log("User Name: ", userName);
 
-    
-})
+    const password = req.body.password;
+    console.log("User password: ", password);
+
+    if(userName === '' && password.length < 9){
+        res.json({
+            message: "Incorrect Details"
+        });
+    }
+    else{
+
+        res.json({
+            message: "Details recieved",
+            userName, password
+        });
+    }
+});
+
+app.post("/Login", (req, res)=>{
+    const userName = req.body.username;
+    console.log("User Name: ", userName);
+
+    const password = req.body.password;
+    console.log("User password: ", password);
+
+    res.json({
+        message: "You are Logged In",
+        userName, password
+    });
+});
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is runnig on http://localhost:${port}`)
+});
