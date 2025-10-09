f3fef60598519b52130de920db5b44eeea5dc3ba
Tani-shido <tanishqdehariya1892004@gmail.com>
2025-10-09 23:31:15 +0530

    Auth-Update

--- Auth-Login-Register(frontend)/src/App.jsx
+++ Auth-Login-Register(frontend)/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
 
 function App() {
 
   return<>
   <div>Hi there</div>
+  <div><Navbar /></div>
+  <div><Main /></div>
   </>
 }
 
