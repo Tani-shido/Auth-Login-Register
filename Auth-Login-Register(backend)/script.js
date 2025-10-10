60cbd433ef99a158c48a5319100b424f2bb92445
Tani-shido <tanishqdehariya1892004@gmail.com>
2025-10-10 23:25:57 +0530

    Auth-update

--- Auth-Login-Register(backend)/script.js
+++ Auth-Login-Register(backend)/script.js
@@ -8,7 +8,6 @@ mongoose.connect(MONGO_URL);
 const mongoose  = require("mongoose");
 const { UserModel } = require("./db.js");
 app.use(express.json());
-const port = 3000;
 
 mongoose.connect(MONGO_URL);
 
@@ -45,56 +44,6 @@ app.post("/Login", (req, res)=>{
         userName, password
     });
 });
-
-
-// const mongoose  = require("mongoose");
-// const { UserModel } = require("./db.js");
-app.use(express.json());
-const port = 3000;
-
-// mongoose.connect("mongodb+srv://shikaridota777:ZClPvfWjJINgzaFN@cluster0.12wqhve.mongodb.net/Auth-Login");
-const mongoose  = require("mongoose");
-const { UserModel } = require("./db.js");
-app.use(express.json());
-const port = 3000;
-
-mongoose.connect(MONGO_URL);
-
-app.post("/Signin", (req,res)=>{
-    const userName = req.body.username;
-    console.log("User Name: ", userName);
-
-    const password = req.body.password;
-    console.log("User password: ", password);
-
-    if(userName === '' && password.length < 9){
-        res.json({
-            message: "Incorrect Details"
-        });
-    }
-    else{
-
-        res.json({
-            message: "Details recieved",
-            userName, password
-        });
-    }
-});
-
-app.post("/Login", (req, res)=>{
-    const userName = req.body.username;
-    console.log("User Name: ", userName);
-
-    const password = req.body.password;
-    console.log("User password: ", password);
-
-    res.json({
-        message: "You are Logged In",
-        userName, password
-    });
-});
-
-
 
 
 app.listen(port, ()=>{
