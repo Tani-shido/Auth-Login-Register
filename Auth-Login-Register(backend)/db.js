9800a6443858633c87252b02e835e097abf643cb
Tani-shido <tanishqdehariya1892004@gmail.com>
2025-10-13 18:04:15 +0530

    Auth-Update

--- Auth-Login-Register(backend)/db.js
+++ Auth-Login-Register(backend)/db.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-const { type } = require("os");
 
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
-const UserDetails = new Schema({
+const UsernameAndPasswords = new Schema({
     username: {type: String},
     password: {type: String}
 });
 
-const UserModel = mongoose.model("UserDetails", UserDetails);
+const UserModel = mongoose.model("UsernameAndPasswords", UsernameAndPasswords);
 
 module.exports = { UserModel }
--- Auth-Login-Register(backend)/script.js
+++ Auth-Login-Register(backend)/script.js
@@ -1,50 +1,111 @@
 const express = require("express");
-const mongoose  = require("mongoose");
-const { UserModel } = require("./db.js");
-const { authMiddleware } = require("./middleware.js");
+const app = express();
 app.use(express.json());
 const port = 3000;
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "MY_SECRET_KEY";
 
-mongoose.connect(MONGO_URL);
 const mongoose  = require("mongoose");
-const { UserModel } = require("./db.js");
-app.use(express.json());
+const { UserModel } = require("./db");
 
-mongoose.connect(MONGO_URL);
-
-app.post("/Signin", (req,res)=>{
-    const userName = req.body.username;
-    console.log("User Name: ", userName);
-
-    const password = req.body.password;
-    console.log("User password: ", password);
-
-    if(userName === '' && password.length < 9){
-        res.json({
-            message: "Incorrect Details"
-        });
+const connectDB = async function (){
+    try{
+        await mongoose.connect("mongodb+srv://shikaridota777:ZClPvfWjJINgzaFN@cluster0.12wqhve.mongodb.net/Auth-Login");
+        console.log("DB connected");
+    }catch(e){
+        console.error(err.message);
     }
-    else{
+}
+connectDB();
 
+
+app.post("/SignUp", async (req,res) =>{
+    try{
+        const userName = req.body.username;
+        const password = req.body.password;
+
+        console.log("User name & password (before validation): ", userName, password);
+// User Name validation
+        if(/^[A-Za-z]+$/.test(userName) === false){
+            res.json({
+                message: "Name can't be empty and must contain only alphabets"
+            });
+        }
+        else{
+// Password validation
+            if(password.length < 5){
+                res.json({
+                    message: "Password must contain more than 5 characters"
+                });
+            }
+            else{
+                console.log("User Name & password (after validation): ", userName, password);
+                try{
+                    const value = await UserModel.create({
+                        username: userName,
+                        password:  password
+                    });
+                    return res.json({
+                        message: "Details recieved and saved in DB",
+                        value
+                    });
+                }
+                catch(error){
+                    console.log("Details not saved in DB");
+                }
+            }
+        }
+    }
+    catch (error){
+        console.log("Details not recieved");
+    }
+});
+
+app.get("/LogIn", async (req, res)=>{
+    try{
+        const userName = req.body.username;
+        const password = req.body.password;
+// User Name validation
+        if(/^[A-Za-z]+$/.test(userName) === false){
+            res.json({
+                message: "Name can't be empty and must contain only alphabets"
+            });
+        }
+        else{
+// Password validation
+            if(password.length < 5){
+                res.json({
+                    message: "Password must contain more than 5 characters"
+                });
+            }
+            else{
+                console.log("User Name & password (after validation) FOR LOGIN: ", userName, password);
+                const findDetails = await UserModel.findOne({
+                    username: userName,
+                    password: password
+                });
+                if(!findDetails){
+                    console.log("User not found");
+                    res.json({
+                        message: "Credentials are wrong"
+                    });
+                }
+                else{
+                    console.log("User found");
+                    res.json({
+                    message: "You are Logged In"
+                    });
+                }
+            }
+        }
+    }
+    catch(e){
         res.json({
-            message: "Details recieved",
-            userName, password
+            message: "Details not recieved"
         });
     }
 });
 
-app.post("/Login", (req, res)=>{
-    const userName = req.body.username;
-    console.log("User Name: ", userName);
-
-    const password = req.body.password;
-    console.log("User password: ", password);
-
-    res.json({
-        message: "You are Logged In",
-        userName, password
-    });
-});
 
 
 app.listen(port, ()=>{
